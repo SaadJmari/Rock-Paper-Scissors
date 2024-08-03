11c4d8cf5e425cba0d7ce7d76b8e2688728dc354
Saad Jmari <55350721+SaadJmari@users.noreply.github.com>
2024-08-03 17:26:54 +0100

    Add player and computer choice function

---
+++ script.js
@@ -0,0 +1,25 @@
+function getComputerChoice() {
+    num = Math.floor(Math.random()* 3) + 1;
+    console.log(num);
+
+    switch(num) {
+        case 1:
+            return "Rock";
+        case 2: 
+            return "Paper";
+        case 3:
+            return "Scissors";
+    }
+}
+
+function getHumanChoice() {
+   let choice = prompt("Choose between: Rock, Paper or Scissors");
+
+
+   while(choice.toLowerCase() !== "rock" && choice.toLowerCase() !== "paper" && choice.toLowerCase() !== "scissors") {
+       choice = prompt("wrong! Choose between: Rock, Paper or Scissors");
+   }
+   return choice;
+   
+}
+
